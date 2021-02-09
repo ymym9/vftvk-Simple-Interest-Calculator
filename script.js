9f7dc0856f53b7aa7fe6754502898ec1ad8e2209
nhuetten <hagen.huetten@rwth-aachen.de>
2021-02-09 14:26:13 +0100

    Main Functionality is working. Layout needs to be refined

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute(){
+    capital = document.getElementById("principal").value;
+    interest_rate = document.getElementById("rate").value;
+	investment_duration = document.getElementById("years").value;
+	
+	
+	if (capital == ""){
+    alert("Please enter the amount of capital you want to invest");
+    principal.focus();
+    return false
+    }
+	else if (investment_duration == ""){
+    alert("Please choose the No. of Years you want to invest");
+    years.focus();
+    return false
+    }
+	else{
+	result = capital * Math.pow((1 + interest_rate * 0.01), investment_duration);
+	current_date = new Date();
+	current_year = current_date.getFullYear();
+	
+	output = "If you deposit <span class='highlighted'>" + capital + "</span>, <br>" + 
+			"at an interest rate of <span class='highlighted'>" + interest_rate + "%</span>. <br>" +
+			"You will receive an amount of <span class='highlighted'>" + result + "</span>, <br>" +
+			"in the year <span class='highlighted'>" + (current_year + parseInt(investment_duration)) + "</span>";
+	
+	document.getElementById("result").innerHTML = output;
+	}
 }
-        
+
+function update_range_value(){
+	
+}
